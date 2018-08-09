76dd404d8b3782aefb0f5a577621c035cdc770c1
Mosh Feu <moshfeu@gmail.com>
2018-08-10 00:12:05 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/pr', (req, res) => {
+  console.log('actual pr requested ----');
+  console.log(req.body);
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
