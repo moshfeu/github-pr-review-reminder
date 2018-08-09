0cf1d0ed54b300840f420938cefdc3b76057ad98
Mosh Feu <moshfeu@gmail.com>
2018-08-09 23:42:41 +0300

    fix build

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at :${port}`);
 });
